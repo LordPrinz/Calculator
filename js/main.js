68e8ab8e6fb123c4b662d8646c5201711c95d945
LordPrinz <plazinski2902@gmail.com>
2021-08-30 19:44:39 +0200

    Creating calculator styling

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+//na 100% da się to lepiej napisać ale nie wiem bruh
+const add = document.querySelector(".button--plus"),
+    substract = document.querySelector(".button--sub"),
+    multiciplacion = document.querySelector(".button--times"),
+    divide = document.querySelector(".button--divide"),
+    procent = document.querySelector(".button--procent"),
+    reverse = document.querySelector(".button--reverse"),
+    power = document.querySelector(".button--power"),
+    sqrt = document.querySelector(".button--sqrt"),
+    buttonCe = document.querySelector(".button--ce"),
+    buttonC = document.querySelector(".button--c"),
+    buttonOne = document.querySelector(".button--1"),
+    buttonTwo = document.querySelector(".button--2"),
+    buttonThree = document.querySelector(".button--3");
